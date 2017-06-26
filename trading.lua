7126b8eeb21abc28f66701aadb5092e4cd6b32ba
ErrorNull <jasonlim@onsiteone.com>
2017-06-26 05:05:04 -0700

    trying out coins idea

--- trading.lua
+++ trading.lua
@@ -1,12 +1,17 @@
-local DEFAULT_ITEMS = { "default:apple", "farming:bread"}
+local DEFAULT_ITEMS = { 
+	{"default:apple 1", "villagers:coins 1"}, 
+	{"farming:bread 1", "villagers:coins 2"} 
+}
 
 villagers.trade = {
 
 	allmende = { DEFAULT_ITEMS }, 
 	
 	bakery = {
-		"farming:flour", "farming:wheat", 
-		"farming:bread", "default:apple"
+		{"farming:flour", "villagers:coins 1"},
+		{"farming:wheat", "villagers:coins 1"},
+		{"farming:bread", "villagers:coins 1"},
+		{"default:apple", "villagers:coins 1"}
 	}, 
 	
 	bench = { DEFAULT_ITEMS }, 
@@ -20,33 +25,52 @@ villagers.trade = {
 	empty16x16 = { DEFAULT_ITEMS }, 
 	
 	farm_full =	{
-		"default:apple", "farming:seed_wheat", "farming:wheat", "farming:straw", 
-		"farming:seed_cotton", "farming:cotton", "farming:string", 
-		"flowers:mushroom_red", "flowers:mushroom_brown", 
+		{"default:apple", "villagers:coins 1"},
+		{"farming:seed_wheat", "villagers:coins 1"},
+		{"farming:wheat", "villagers:coins 1"},
+		{"farming:straw", "villagers:coins 1"},
+		{"farming:seed_cotton", "villagers:coins 1"},
+		{"farming:cotton", "villagers:coins 1"},
+		{"farming:string", "villagers:coins 1"},
+		{"flowers:mushroom_red", "villagers:coins 1"},
+		{"flowers:mushroom_brown", "villagers:coins 1"}
 	}, 
 	
 	farm_tiny = {
-		"farming:wheat", "farming:cotton", 
-		"flowers:rose", "flowers:tulip", "flowers:dandelion_yellow", 
-		"flowers:geranium", "flowers:viola", "flowers:dandelion_white"
+		{"farming:wheat", "villagers:coins 1"},
+		{"farming:cotton", "villagers:coins 1"},
+		{"flowers:rose", "villagers:coins 1"},
+		{"flowers:tulip", "villagers:coins 1"},
+		{"flowers:dandelion_yellow", "villagers:coins 1"},
+		{"flowers:geranium", "villagers:coins 1"},
+		{"flowers:viola", "villagers:coins 1"},
+		{"flowers:dandelion_white", "villagers:coins 1"}
 	}, 
 	
 	field = {
-		"farming:wheat", "farming:cotton", "default:dirt", "default:clay",
+		{"farming:wheat", "villagers:coins 1"}
+		{"farming:cotton", "villagers:coins 1"}
+		{"default:dirt", "villagers:coins 1"}
+		{"default:clay", "villagers:coins 1"}
 	}, 
 	
 	forge = {
-		"default:iron_lump", "default:steelblock", 
-		"default:copper_lump", "default:copperblock",
-		"default:tin_lump", "default:tinblock",
-		"default:gold_lump", "default:goldblock",
-		"default:bronzeblock", "default:sword_steel", 
-		"default:sword_bronze", 
-		"default:steel_ingot",
-		"default:copper_ingot",
-		"default:tin_ingot",
-		"default:bronze_ingot",
-		"default:gold_ingot"
+		{"default:iron_lump", "villagers:coins 1"},
+		{"default:steelblock", "villagers:coins 1"},
+		{"default:copper_lump", "villagers:coins 1"},
+		{"default:copperblock", "villagers:coins 1"},
+		{"default:tin_lump", "villagers:coins 1"},
+		{"default:tinblock", "villagers:coins 1"},
+		{"default:gold_lump", "villagers:coins 1"},
+		{"default:goldblock", "villagers:coins 1"},
+		{"default:bronzeblock", "villagers:coins 1"},
+		{"default:sword_steel", "villagers:coins 1"},
+		{"default:sword_bronze", "villagers:coins 1"},
+		{"default:steel_ingot", "villagers:coins 1"},
+		{"default:copper_ingot", "villagers:coins 1"},
+		{"default:tin_ingot", "villagers:coins 1"},
+		{"default:bronze_ingot", "villagers:coins 1"},
+		{"default:gold_ingot", "villagers:coins 1"}
 	}, 
 	
 	fountain = { DEFAULT_ITEMS }, 
@@ -55,69 +79,110 @@ villagers.trade = {
 	library = { DEFAULT_ITEMS }, 
 	
 	lumberjack = {
-		"default:tree", "default:jungletree", "default:pine_tree", 
-		"default:acacia_tree", "default:acacia_tree", "default:aspen_tree",
+		{"default:tree", "villagers:coins 1"},
+		{"default:jungletree", "villagers:coins 1"},
+		{"default:pine_tree", "villagers:coins 1"},
+		{"default:acacia_tree", "villagers:coins 1"},
+		{"default:acacia_tree", "villagers:coins 1"},
+		{"default:aspen_tree", "villagers:coins 1"}
 	}, 
 	
 	mill = {
-		"farming:wheat", "farming:straw", "farming:flour", 
+		{"farming:wheat", "villagers:coins 1"},
+		{"farming:straw", "villagers:coins 1"},
+		{"farming:flour", "villagers:coins 1"}
 	}, 
 	
 	pasture = {
-		"flowers:rose", "flowers:tulip", "flowers:dandelion_yellow", 
-		"flowers:geranium", "flowers:viola", "flowers:dandelion_white"
+		{"flowers:rose", "villagers:coins 1"},
+		{"flowers:tulip", "villagers:coins 1"},
+		{"flowers:dandelion_yellow", "villagers:coins 1"},
+		{"flowers:geranium", "villagers:coins 1"},
+		{"flowers:viola", "villagers:coins 1"},
+		{"flowers:dandelion_white", "villagers:coins 1"}
 	}, 
 	
 	pit = {
-		"default:dirt", "default:sand", "default:gravel"
+		{"default:dirt", "villagers:coins 1"},
+		{"default:sand", "villagers:coins 1"},
+		{"default:gravel", "villagers:coins 1"}
 	}, 
 	
 	sawmill = {
-		"default:wood", "default:junglewood", "default:pine_wood", 
-		"default:acacia_wood", "default:acacia_wood", "default:aspen_wood"
+		{"default:wood", "villagers:coins 1"},
+		{"default:junglewood", "villagers:coins 1"},
+		{"default:pine_wood", "villagers:coins 1"},
+		{"default:acacia_wood", "villagers:coins 1"},
+		{"default:acacia_wood", "villagers:coins 1"},
+		{"default:aspen_wood", "villagers:coins 1"}
 	}, 
 	
 	school = { DEFAULT_ITEMS }, 
 	secular = { DEFAULT_ITEMS }, 
 	shed =	{ DEFAULT_ITEMS }, 
 	shop = { 
-		"default:key", "default:torch",
-		"default:shovel_steel", "default:shovel_bronze", "default:shovel_mese", "default:shovel_diamond",
-		"default:pick_steel", "default:pick_bronze", "default:pick_mese", "default:pick_diamond",
-		"default:sword_steel", "default:sword_bronze", "default:sword_mese", "default:sword_diamond",
-		"default:axe_steel", "default:axe_bronze", "default:axe_mese", "default:axe_diamond",
-		"default:cactus", "default:papyrus", "default:chest_locked", "default:sign_wall_steel", "default:ladder_steel",
-		"default:glass", "default:meselamp", "default:paper", "default:book", "default:skeleton_key", "default:flint", 
-		"carts:rail", "carts:cart", "beds:fancy_bed", "beds:bed", "boats:boat", "bucket:bucket_empty", 
+		{"default:key", "villagers:coins 1"},
+		{"default:torch", "villagers:coins 1"},
+		{"default:shovel_steel", "villagers:coins 1"},
+		{"default:shovel_bronze", "villagers:coins 1"},
+		{"default:shovel_mese", "villagers:coins 1"},
+		{"default:shovel_diamond", "villagers:coins 1"},
+		{"default:pick_steel", "villagers:coins 1"},
+		{"default:pick_bronze", "villagers:coins 1"},
+		{"default:pick_mese", "villagers:coins 1"},
+		{"default:pick_diamond", "villagers:coins 1"},
+		{"default:sword_steel", "villagers:coins 1"},
+		{"default:sword_bronze",  "villagers:coins 1"},
+		{"default:sword_mese", "villagers:coins 1"},
+		{"default:sword_diamond", "villagers:coins 1"},
+		{"default:axe_steel", "villagers:coins 1"},
+		{"default:axe_bronze", "villagers:coins 1"},
+		{"default:axe_mese", "villagers:coins 1"},
+		{"default:axe_diamond", "villagers:coins 1"},
+		{"default:cactus", "villagers:coins 1"},
+		{"default:papyrus", "villagers:coins 1"},
+		{"default:chest_locked", "villagers:coins 1"},
+		{"default:sign_wall_steel", "villagers:coins 1"},
+		{"default:ladder_steel", "villagers:coins 1"},
+		{"default:glass", "villagers:coins 1"},
+		{"default:meselamp", "villagers:coins 1"},
+		{"default:paper", "villagers:coins 1"},
+		{"default:book", "villagers:coins 1"},
+		{"default:skeleton_key", "villagers:coins 1"},
+		{"default:flint", "villagers:coins 1"},
+		{"carts:rail", "villagers:coins 1"},
+		{"carts:cart", "villagers:coins 1"},
+		{"beds:fancy_bed", "villagers:coins 1"},
+		{"beds:bed", "villagers:coins 1"},
+		{"boats:boat", "villagers:coins 1"},
+		{"bucket:bucket_empty", "villagers:coins 1"}
 	}, 
 	spawn = { DEFAULT_ITEMS }, 
 	tavern = { DEFAULT_ITEMS }, 
 	tent = { DEFAULT_ITEMS }, 
 	
-	tower =	{ "default:sword_steel", "default:sword_bronze" }, 
+	tower =	{ 
+		{"default:sword_steel", "villagers:coins 1"},
+		{"default:sword_bronze", "villagers:coins 1"}
+	}, 
 	
 	trader = {
-		"default:clay", "default:clay_lump"
+		{"default:clay", "villagers:coins 1"},
+		{"default:clay_lump", "villagers:coins 1"}
 	}, 
 	
 	village_square = { DEFAULT_ITEMS }, 
 	wagon = { DEFAULT_ITEMS }, 
-	well = { DEFAULT_ITEMS }
+	well = { DEFAULT_ITEMS },
 
+	-- village towntest: castle
+	castle = { DEFAULT_ITEMS },
+	park = { DEFAULT_ITEMS }, 
+
+	-- village gambit: cementry, lamp, hotel, pub, stable, 
+	cementry = { DEFAULT_ITEMS },
+	lamp = { DEFAULT_ITEMS },
+	hotel = { DEFAULT_ITEMS },
+	pub = { DEFAULT_ITEMS },
+	stable = { DEFAULT_ITEMS }
 }
-
---[[
-"farming:seed_wheat", "farming:wheat", "default:stick", 
-"farming:seed_cotton", "farming:cotton",
-"default:dirt", "default:gravel",
-
-"farming:straw", "farming:flour", "farming:bread", "farming:cotton",
-"farming:string", "bucket:bucket_empty", "default:dirt", "default:sand", "default:gravel", "default:clay",
-"default:leaves", "default:wood", "default:cobble", "default:apple", "default:stick", "default:clay_lump",
-"default:tree", "default:jungleleaves", "default:pine_needles", "default:acacia_leaves", "default:aspen_leaves",
-"default:dry_shrub", "default:bush_stem", "default:bush_leaves", "default:acacia_bush_stem", "default:acacia_bush_leaves",
-"farming:seed_wheat", "farming:seed_cotton", 
-
-"flowers:waterlily", "flowers:mushroom_red", "flowers:mushroom_brown", 
-"flowers:rose", "flowers:tulip", "flowers:dandelion_yellow", "flowers:geranium", "flowers:viola", "flowers:dandelion_white"
---]]
