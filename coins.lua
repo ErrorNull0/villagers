6aff54e027da995a344c3616074d58f200726523
LouisCyfer <LouisCyfer@users.noreply.github.com>
2017-08-18 17:29:47 +0200

    Update coins.lua
    -- another hotfix for dirt drops, if coins and "ethereal:dry_dirt" are involved (this time it's working!)

--- coins.lua
+++ coins.lua
@@ -1,18 +1,22 @@
 local enable_coin_drop = minetest.settings:get_bool("coin_drop")
 if enable_coin_drop == nil then enable_coin_drop = true end
 
--- coin findings in dirty nodes :P
-local item_drop_def = {
-	{ items = {"villagers:coins_gold", "default:dirt"}, rarity = 50 },
-	{ items = {"villagers:coins", "default:dirt"}, rarity = 30 }
-}
-
 local coin_nodes = { "default:dirt", "default:dirt_with_grass", "default:dirt_with_dry_grass", "default:dirt_with_rainforest_litter", "default:dirt_with_snow" }
 local ethereal_found = false
 
 for i, name in ipairs(minetest.get_modnames()) do
 	if name == "ethereal" then ethereal_found = true
 	end
+end
+
+-- coin findings in dirty nodes :P
+local function coin_drop_items(node)
+	local set_dirt = "default:dirt"
+
+	-- special treatment for "ethereal:dry_dirt" because it has no drop value, so it gets one :)
+	if node == "ethereal:dry_dirt" then set_dirt = node end
+
+	return { { items = {"villagers:coins_gold", set_dirt}, rarity = 50 }, { items = {"villagers:coins", set_dirt}, rarity = 30 } }
 end
 
 if enable_coin_drop then
@@ -42,34 +46,23 @@ if enable_coin_drop then
 			local debug
 			local set_max_items = 1
 
-			-- reset table
-			local new_items = {}
-			for idx = 1, #item_drop_def do table.insert(new_items, item_drop_def[idx]) end
-
-			new_items[1].items[2] = "default:dirt"
-			new_items[2].items[2] = "default:dirt"
+			-- getting drop-item-table
+			local new_items = coin_drop_items(coin_nodes[n])
 
 			-- default drop = "string", build a table and include the default drop at last position
 			if type(item_def.drop) == "string" or coin_nodes[n] == "ethereal:dry_dirt" then
 				debug = " --> creating new table"
-
 				local insert_drop = item_def.drop
 
 				-- special treatment for "ethereal:dry_dirt" because it has no drop value, so it gets one :)
-				if coin_nodes[n] == "ethereal:dry_dirt" then
-					new_items[1].items[2] = "ethereal:dry_dirt"
-					new_items[2].items[2] = "ethereal:dry_dirt"
-					insert_drop = coin_nodes[n]
-				end
+				if coin_nodes[n] == "ethereal:dry_dirt" then insert_drop = coin_nodes[n] end
 
 				table.insert(new_items, { items = { insert_drop } })
-			elseif type(item_def.drop) == "table" then
 
+			elseif type(item_def.drop) == "table" then
 				debug = " --> modifying existing table"
 				set_max_items = item_def.drop.max_items
-				for idx = 1, #item_def.drop.items do
-					table.insert(new_items, item_def.drop.items[idx])
-				end
+				for idx = 1, #item_def.drop.items do table.insert(new_items, item_def.drop.items[idx]) end
 			end
 
 			local set_item_drop = {
